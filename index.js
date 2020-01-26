9c7cae9c9fcf79af11cfd1958bd4e504e7fb1279
Frank Moran <info@francisfrankmoran.com>
2020-01-26 13:06:51 -0500

    grabbing specific values from response object, everything outside of number of stars

--- index.js
+++ index.js
@@ -15,22 +15,51 @@ inquirer.prompt([
         name: "username"    
     }
 ]).then(function(response){
-    console.log(response.color);
-    console.log(response.username);
+    // Store user's favorite color and their entered 
+    // username into variables
+    var color = response.color;
+    var username = response.username;
 
-    // $.ajax({
-    //     url: "https://api.github.com/users/" + response.username,
-    //     method: "GET"
-    // }).then(function(gitResponse){
-    //     console.log(gitResponse);
-    // });
+    axios.get("https://api.github.com/users/" + response.username).catch(function(error){
+        console.log("no user found");
+    }).then(function(gitResponse){
 
-    axios.get("https://api.github.com/users/" + response.username).then(function(gitResponse){
-        console.log(gitResponse)
-    }).catch(function(error){
-        console.log(error);
-    }).finally(function(){
+        // console.log(gitResponse)
+        // Profile image
+        console.log(gitResponse.data.login);
 
+        // User name
+        console.log(gitResponse.data.avatar_url);
+
+        // Links to the following:
+        // User location via Google Maps
+        console.log(gitResponse.data.location);
+
+        // User GitHub profile
+        console.log(gitResponse.data.html_url);
+
+        // User blog
+        console.log(gitResponse.data.blog);
+
+        // User bio
+        console.log(gitResponse.data.bio);
+
+        // Number of public repositories
+        console.log(gitResponse.data.public_repos);
+
+        // Number of followers
+        console.log(gitResponse.data.followers);
+
+        // Number of GitHub stars
+        // console.log(gitResponse.data.);
+
+        // Number of users following
+        console.log(gitResponse.data.following);
+
+        // Get list of stars
+        // axios.get("https://api.github.com/users/" + response.username + "/starred").then(function(starResponse){
+        //     console.log(starResponse);
+        // });
     });
 
 
