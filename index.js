72c7049a6f131aabead8d19ecdf8bf09e3dc555f
Frank Moran <info@francisfrankmoran.com>
2020-01-26 17:57:39 -0500

    star count attempt

--- index.js
+++ index.js
@@ -24,41 +24,42 @@ inquirer.prompt([
         console.log("no user found");
     }).then(function(gitResponse){
 
-        // console.log(gitResponse)
-        // Profile image
-        console.log(gitResponse.data.login);
+        // // console.log(gitResponse)
+        // // Profile image
+        // console.log(gitResponse.data.login);
 
-        // User name
-        console.log(gitResponse.data.avatar_url);
+        // // User name
+        // console.log(gitResponse.data.avatar_url);
 
-        // Links to the following:
-        // User location via Google Maps
-        console.log(gitResponse.data.location);
+        // // Links to the following:
+        // // User location via Google Maps
+        // console.log(gitResponse.data.location);
 
-        // User GitHub profile
-        console.log(gitResponse.data.html_url);
+        // // User GitHub profile
+        // console.log(gitResponse.data.html_url);
 
-        // User blog
-        console.log(gitResponse.data.blog);
+        // // User blog
+        // console.log(gitResponse.data.blog);
 
-        // User bio
-        console.log(gitResponse.data.bio);
+        // // User bio
+        // console.log(gitResponse.data.bio);
 
-        // Number of public repositories
-        console.log(gitResponse.data.public_repos);
+        // // Number of public repositories
+        // console.log(gitResponse.data.public_repos);
 
-        // Number of followers
-        console.log(gitResponse.data.followers);
+        // // Number of followers
+        // console.log(gitResponse.data.followers);
 
-        // Number of GitHub stars
-        // console.log(gitResponse.data.);
+        // // Number of GitHub stars
+        // // console.log(gitResponse.data.);
 
-        // Number of users following
-        console.log(gitResponse.data.following);
+        // // Number of users following
+        // console.log(gitResponse.data.following);
 
         // Get list of stars
         // axios.get("https://api.github.com/users/" + response.username + "/starred").then(function(starResponse){
-        //     console.log(starResponse);
+        //     var starObject = JSON.parse(starResponse);
+        //     console.log(starObject.length);
         // });
     });
 
