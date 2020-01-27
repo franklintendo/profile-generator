0eeb0fcf27972b27a4b2cc14dbf54635bbfbab52
Frank Moran <info@francisfrankmoran.com>
2020-01-27 12:04:50 -0500

    npm install html5-to-pdf

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 var inquirer = require("inquirer");
 var axios = require("axios");
+const HTML5ToPDF = require("../lib")
+const path = require("path")
+
+const run = async () => {
+  const html5ToPDF = new HTML5ToPDF({
+    inputPath: path.join(__dirname, "assets", "basic.html"),
+    outputPath: path.join(__dirname, "..", "tmp", "output.pdf"),
+    templatePath: path.join(__dirname, "templates", "basic"),
+    include: [
+      path.join(__dirname, "assets", "basic.css"),
+      path.join(__dirname, "assets", "custom-margin.css"),
+    ],
+  })
+
+  await html5ToPDF.start()
+  await html5ToPDF.build()
+  await html5ToPDF.close()
+  console.log("DONE")
+  process.exit(0)
+}
 
 
 inquirer.prompt([
