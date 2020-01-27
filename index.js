7c6e0f1f6e6fcd4aa0ae0f883f326fae4b7ff103
Frank Moran <info@francisfrankmoran.com>
2020-01-26 19:02:34 -0500

    html-to-pdf, ca-pdf, html-pdf command not working...

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var inquirer = require("inquirer");
 var axios = require("axios");
+var fs = require('fs');
+var pdf = require('html-pdf');
+var html = fs.readFileSync('./pdf-template.html', 'utf8');
+var options = { format: 'Letter' };
 
 
 inquirer.prompt([
@@ -25,41 +29,51 @@ inquirer.prompt([
     }).then(function(gitResponse){
 
         // console.log(gitResponse)
-        // Profile image
-        console.log(gitResponse.data.login);
-
         // User name
-        console.log(gitResponse.data.avatar_url);
+        // console.log(gitResponse.data.login);
 
+        // Profile pic
+        console.log(gitResponse.data.avatar_url);
+        
+        module.exports = {
+            src: gitResponse.data.avatar_url
+        }
         // Links to the following:
         // User location via Google Maps
-        console.log(gitResponse.data.location);
+        // console.log(gitResponse.data.location);
 
         // User GitHub profile
-        console.log(gitResponse.data.html_url);
+        // console.log(gitResponse.data.html_url);
 
         // User blog
-        console.log(gitResponse.data.blog);
+        // console.log(gitResponse.data.blog);
 
         // User bio
-        console.log(gitResponse.data.bio);
+        // console.log(gitResponse.data.bio);
 
         // Number of public repositories
-        console.log(gitResponse.data.public_repos);
+        // console.log(gitResponse.data.public_repos);
 
         // Number of followers
-        console.log(gitResponse.data.followers);
+        // console.log(gitResponse.data.followers);
 
         // Number of GitHub stars
         // console.log(gitResponse.data.);
 
         // Number of users following
-        console.log(gitResponse.data.following);
+        // console.log(gitResponse.data.following);
 
         // Get list of stars
         // axios.get("https://api.github.com/users/" + response.username + "/starred").then(function(starResponse){
         //     console.log(starResponse);
         // });
+
+        // pdf.create(html, options).toFile('./github-user.pdf', function(err, res) {
+        //     if (err) return console.log(err);
+        //     console.log(res); // { filename: '/github-user.pdf' }
+        //   });
+
+        
     });
 
 
